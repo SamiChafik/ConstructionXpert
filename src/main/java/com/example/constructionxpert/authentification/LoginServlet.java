bad7fe0ae8f69e4bd0b25661a3d711fe52d09e08
SamiChafik <sami.chafik00@gmail.com>
2025-03-17 15:45:36 +0000

    changed a path

--- src/main/java/com/example/constructionxpert/authentification/LoginServlet.java
+++ src/main/java/com/example/constructionxpert/authentification/LoginServlet.java
@@ -36,7 +36,7 @@ private Table table;
         boolean isValidUser = userDAO.checkLogin(email, password);
 
         if (isValidUser) {
-            response.sendRedirect("dashboard.jsp");
+            response.sendRedirect("addProject.jsp");
         } else {
             response.sendRedirect("Login.jsp?error=invalid_credentials");
         }
