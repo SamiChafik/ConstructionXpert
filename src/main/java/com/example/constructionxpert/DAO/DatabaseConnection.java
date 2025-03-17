44804e7d812c1edf7c948924fcf854cba5955a13
SamiChafik <sami.chafik00@gmail.com>
2025-03-17 15:26:10 +0000

    added jdbc driver

--- src/main/java/com/example/constructionxpert/DAO/DatabaseConnection.java
+++ src/main/java/com/example/constructionxpert/DAO/DatabaseConnection.java
@@ -7,12 +7,16 @@ public class DatabaseConnection {
     private String jdbcURL = "jdbc:mysql://localhost:3306/constructionxpert";
     private String jdbcUsername = "root";
     private String jdbcPassword = "123456789";
+    private String jdbcDriver = "com.mysql.cj.jdbc.Driver";
 
     protected java.sql.Connection getConnection() {
         try {
+            Class.forName(jdbcDriver);
             return DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
         } catch (SQLException e) {
             throw new RuntimeException("Failed to connect to the database: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
